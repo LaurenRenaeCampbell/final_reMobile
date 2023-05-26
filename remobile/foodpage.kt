8c8e2899f0101c29f8be12bf2a87e329d0abfb60
Lauren Campbell <62973230+LaurenRenaeCampbell@users.noreply.github.com>
2023-05-26 14:50:06 -0700

    Update foodpage.kt

--- remobile/foodpage.kt
+++ remobile/foodpage.kt
@@ -3,6 +3,7 @@ package com.example.remobile
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.ImageButton
 
 class foodpage : AppCompatActivity() {
@@ -13,6 +14,12 @@ class foodpage : AppCompatActivity() {
         val food_back = findViewById<ImageButton>(R.id.foodbackbutton)
         food_back.setOnClickListener{
             val intent = Intent(this,homepage_campus::class.java)
+            startActivity(intent)
+        }
+
+        val foodcourt_but = findViewById<Button>(R.id.foodcourt_but)
+        foodcourt_but.setOnClickListener{
+            val intent = Intent(this,foodcourtinfo::class.java)
             startActivity(intent)
         }
     }
